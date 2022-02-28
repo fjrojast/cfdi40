55c82b17e2d892137f60f3608e6b7f9639663e88
Fernando Rivero <frivero@itadmin.com.mx>
2022-02-28 17:51:33 -0500

    [FIX] local taxes

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -540,16 +540,16 @@ class AccountMove(models.Model):
            if tax_local_tras and not tax_local_ret:
                request_params.update({'implocal10': {'TotaldeTraslados': self.set_decimals(tax_local_tras_tot, 2),
                                                      'TotaldeRetenciones': self.set_decimals(tax_local_ret_tot,2), 
-                                                     'TrasladosLocales': self.set_decimals(tax_local_tras,2),}})
+                                                     'TrasladosLocales': tax_local_tras,}})
            if tax_local_ret and not tax_local_tras:
                request_params.update({'implocal10': {'TotaldeTraslados': self.set_decimals(tax_local_tras_tot,2), 
                                                      'TotaldeRetenciones': self.set_decimals(tax_local_ret_tot * -1,2), 
-                                                     'RetencionesLocales': self.set_decimals(tax_local_ret,2),}})
+                                                     'RetencionesLocales': tax_local_ret,}})
            if tax_local_ret and tax_local_tras:
                request_params.update({'implocal10': {'TotaldeTraslados': self.set_decimals(tax_local_tras_tot,2),
                                                      'TotaldeRetenciones': self.set_decimals(tax_local_ret_tot * -1,2),
-                                                     'TrasladosLocales': self.set_decimals(tax_local_tras,2),
-                                                     'RetencionesLocales': self.set_decimals(tax_local_ret,2),}})
+                                                     'TrasladosLocales': tax_local_tras,
+                                                     'RetencionesLocales': tax_local_ret,}})
 
         if self.tipo_comprobante == 'T':
             request_params['factura'].update({'subtotal': '0.00','total': '0.00'})
@@ -881,7 +881,7 @@ class AccountMove(models.Model):
                    return
 
                json_response = response.json()
-               #_logger.info('something ... %s', response.text)
+             #  _logger.info('something ... %s', response.text)
             except Exception as e:
                _logger.info('log de la exception ... %s', response.text)
                json_response = {}
